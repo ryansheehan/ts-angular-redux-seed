7d932639e9c94193ae3dde827d2c54ac3e6f0a8c
Ryan Sheehan <rsheehan@gmail.com>
2016-07-10 10:05:53 -0500

    added redux logging

--- app/config/store.ts
+++ app/config/store.ts
@@ -1,7 +1,10 @@
 import {INgReduxProvider} from 'ng-redux';
 import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
 import rootReducer from '../reducers/index';
 
 export default function createStore($ngReduxProvider: INgReduxProvider):void {
-    $ngReduxProvider.createStoreWith(rootReducer, ['ngUiRouterMiddleware', thunk], []);
+    const logger = createLogger();
+
+    $ngReduxProvider.createStoreWith(rootReducer, ['ngUiRouterMiddleware', thunk, logger], []);
 }
