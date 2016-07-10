568216a5ed9b31ceb318ccb8127298dfe0b846cb
Ryan Sheehan <rsheehan@gmail.com>
2016-07-10 10:41:31 -0500

    removed logging of router message

--- app/config/store.ts
+++ app/config/store.ts
@@ -1,10 +1,20 @@
+import {Action} from 'redux';
 import {INgReduxProvider} from 'ng-redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import rootReducer from '../reducers/index';
 
 export default function createStore($ngReduxProvider: INgReduxProvider):void {
-    const logger = createLogger();
+    const logger = createLogger({
+        predicate: (getState: any, action: Action) => {
+            switch(action.type) {
+                case '@@reduxUiRouter/$stateChangeStart':
+                case '@@reduxUiRouter/$stateChangeSuccess':
+                    return false;
+            }
+            return true;
+        }
+    });
 
     $ngReduxProvider.createStoreWith(rootReducer, ['ngUiRouterMiddleware', thunk, logger], []);
 }
