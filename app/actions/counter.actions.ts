3d585f9169c50920bf5f3e074eaa70bad74e0464
Ryan Sheehan <rsheehan@gmail.com>
2016-07-10 22:21:52 -0500

    Slight change to dispatch of actions

--- app/actions/counter.actions.ts
+++ app/actions/counter.actions.ts
@@ -4,27 +4,30 @@ import {ICounterState} from '../state/counter.state';
 export const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
 export const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
 
-export function increment(): Action {
+interface CounterAction extends Action {}
+
+export function increment(): CounterAction {
     return {type: INCREMENT_COUNTER};
 }
 
-export function decrement(): Action {
+export function decrement(): CounterAction {
     return {type: DECREMENT_COUNTER};
 }
 
 export function incrementIfOdd() {
-    return (dispatch:Dispatch<Action>, getState: ()=>ICounterState) => {
+    return (dispatch:Dispatch<CounterAction>, getState: ()=>ICounterState) => {
         const {counter} = getState();
 
         if(counter % 2 == 0) {
             return;
         }
-        dispatch(increment());
+
+        dispatch(increment);
     }
 }
 
 export function incrementAsync(delay = 1000) {
     return (dispatch:Dispatch<Action>):void => {
-        setTimeout(() => dispatch(increment()), delay);
+        setTimeout(() => dispatch(increment), delay);
     }
 }
