f80bae22dab7dc0a634c9e31b87eafc71cac6699
ryan sheehan <rsheehan@gmail.com>
2016-07-13 15:45:16 -0500

    added updating a user

--- app/actions/users.actions.ts
+++ app/actions/users.actions.ts
@@ -4,17 +4,23 @@ import {/*IUserListState,*/ IUserState} from '../state/users.state';
 
 export const ADD_USER = 'ADD_USER';
 export const DELETE_USER = 'DELETE_USER';
-//export const EDIT_USER_NAME = 'EDIT_USER_NAME';
-//export const EDIT_USER_PASSWORD = 'EDIT_USER_PASSWORD';
+export const UPDATE_USER = 'UPDATE_USER';
 
 export interface UsersListAction extends Action {
-    payload: IUserState;
+    payload: {
+        newUser: IUserState,
+        oldUser: IUserState
+    };
 }
 
 export function addUser(user:IUserState): UsersListAction {
-    return { type: ADD_USER, payload: user };
+    return { type: ADD_USER, payload: {newUser: user, oldUser: undefined} };
 }
 
 export function deleteUser(user:IUserState): UsersListAction {
-    return {type: DELETE_USER, payload: user};
+    return {type: DELETE_USER, payload: {newUser: undefined, oldUser: user}};
+}
+
+export function updateUser(oldUser:IUserState, newUser:IUserState): UsersListAction {
+    return {type: UPDATE_USER, payload: {oldUser, newUser}};
 }
--- app/reducers/users.reducer.ts
+++ app/reducers/users.reducer.ts
@@ -1,34 +1,53 @@
-import {ADD_USER, DELETE_USER, UsersListAction} from '../actions/users.actions';
+import {ADD_USER, DELETE_USER, UPDATE_USER, UsersListAction} from '../actions/users.actions';
 import {IUserState} from '../state/users.state';
 
+function checkUserDataValid(user:IUserState): boolean {
+    return user && user.username && user.password
+}
+
 function addUser(state: IUserState[], action: UsersListAction): IUserState[] {
-    if(!(action.payload && action.payload.username && action.payload.password)) {
+    if(!(action.payload && checkUserDataValid(action.payload.newUser))) {
         return state;
     }
 
     let found = false;
     for(var i = 0; i < state.length && !found; i++) {
-        if(state[i].username === action.payload.username) {
+        if(state[i].username === action.payload.newUser.username) {
             found = true;
         }
     }
 
     if(!found) {
-        return state.concat(action.payload);
+        return state.concat(action.payload.newUser);
     }
 
     return state;
 }
 
 function removeUser(state: IUserState[] = [], action: UsersListAction): IUserState[] {
-    if(!(action.payload && action.payload.username)) {
+    if(!(action.payload && checkUserDataValid(action.payload.oldUser))) {
         return state;
     }
 
     let found = false;
     for(var i = 0; i < state.length && !found; i++) {
-        if(state[i].username == action.payload.username) {
+        if(state[i].username == action.payload.oldUser.username) {
             return [...state.slice(0, i), ...state.slice(i+1)];
+        }
+    }
+
+    return state;
+}
+
+function updateUser(state: IUserState[] = [], action: UsersListAction): IUserState[] {
+    if(action.payload && checkUserDataValid(action.payload.newUser) && checkUserDataValid(action.payload.oldUser)) {
+        return state;
+    }
+
+    let found = false;
+    for(var i = 0; i < state.length && !found; i++) {
+        if(state[i].username === action.payload.oldUser.username) {
+            return [...state.slice(0, i), action.payload.newUser, ...state.slice(i+1)];
         }
     }
 
@@ -39,7 +58,7 @@ export default function usersReducer(state: IUserState[] = [], action: UsersList
     switch(action.type) {
         case ADD_USER:      return addUser(state, action);
         case DELETE_USER:   return removeUser(state, action);
-
+        case UPDATE_USER:   return updateUser(state, action);
         default:            return state;
     }
 }
