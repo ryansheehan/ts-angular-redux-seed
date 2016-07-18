44f5443f306a8fc3f7224c9afdfe3753f7b5909a
Ryan Sheehan <rsheehan@gmail.com>
2016-07-18 01:46:15 -0500

    initial check in for a route rule system

--- app/config/routes/index.ts
+++ app/config/routes/index.ts
@@ -1,4 +1,4 @@
-import {IStateProvider} from "angular-ui-router";
+import {IStateProvider} from 'angular-ui-router';
 import {HomeRoute} from './home.route';
 import {AboutRoute} from './about.route';
 import {UsersRoute} from './users.route';
@@ -6,22 +6,15 @@ import {UserRoute} from './user.route';
 
 export {HOME_ROUTE} from './home.route';
 
+let states = [
+    new HomeRoute(),
+    new AboutRoute(),
+    new UsersRoute(),
+    new UserRoute()
+];
+
 export function registerRoutes($stateProvider: IStateProvider) {
-    $stateProvider.state(new HomeRoute());
-    $stateProvider.state(new AboutRoute());
-    $stateProvider.state(new UsersRoute());
-    $stateProvider.state(new UserRoute());
+    states.forEach((state) => $stateProvider.state(state));
 }
 
 
-// $rootScope.$on('$stateChangeStart',
-//     (e: IAngularEvent,
-//      toState: IState,
-//      toParams: IStateParamsService,
-//      fromState: IState,
-//      fromParams: IStateParamsService,
-//      options: IStateOptions):void => {
-//
-//         console.log({e, toState, toParams, fromState, fromParams, options});
-//
-// })
---
+++ app/config/routes/rule.manager.ts
@@ -0,0 +1,55 @@
+import {IRootScopeService, IAngularEvent} from 'angular';
+import {IState, IStateParamsService, IStateOptions} from 'angular-ui-router';
+import {INgRedux} from 'ng-redux';
+import {stateGo} from 'redux-ui-router';
+
+export interface IRouteRuleResult {
+    redirect: boolean,
+    name?: string,
+    params?: IStateParamsService,
+    options?: IStateOptions
+}
+
+export interface IRouteRule extends Function {
+    ($ngRedux: INgRedux,
+     toState: IState,
+     toParams: IStateParamsService,
+     fromState: IState,
+     fromParams: IStateParamsService,
+     options: IStateOptions): IRouteRuleResult
+}
+
+export interface IDataRouteRules {
+    rules?: IRouteRule[]
+}
+
+export interface IRuleState extends IState {
+    data?: IDataRouteRules
+}
+
+class RouteRuleManager {
+    static $inject = ['$rootScope', '$ngRedux'];
+
+    constructor($rootScope: IRootScopeService, $ngRedux: INgRedux) {
+        $rootScope.$on('$stateChangeStart',
+            (e: IAngularEvent,
+             toState: IState,
+             toParams: IStateParamsService,
+             fromState: IState,
+             fromParams: IStateParamsService,
+             options: IStateOptions) => {
+                let state = toState as IRuleState;
+                if(state && state.data && state.data.rules) {
+                    let rules = state.data.rules;
+                    for(var i = 0; i < rules.length; i++) {
+                        let rule = rules[i];
+                        let result = rule($ngRedux, toState, toParams, fromState, fromParams, options);
+                        if(result.redirect) {
+                            stateGo(result.name, result.params, result.options);
+                        }
+                    }
+                }
+            }
+        );
+    }
+}
